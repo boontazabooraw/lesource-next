6b1d2e752fdbc87966856138365637d065bfcb98
Leon Bryan <leonard.bryan.email@gmail.com>
2026-03-17 20:53:46 +0800

    REFACTOR: refactored the imports, same functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
 import Tabs from "@/components/Tabs";
+import Wares from "@/components/TabContents/Wares";
 
 export default function Home() {
+
+  const tabData = [
+    { title: "Wares", content: <Wares /> },
+    { title: "Guides", content: <div>Guides content here</div> },
+    { title: "About", content: <div>About content here</div> },
+  ];
+
   return (
     <div>
-      <Tabs />
+      <Tabs tabData={tabData} />
     </div>
   );
 }
--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -3,18 +3,14 @@
 import React, { useState } from 'react'
 import TabPanel from './TabPanel';
 
-const tabData = [
-    { title: "Wares", content: <div>Wares content here</div> },
-    { title: "Guides", content: <div>Guides content here</div> },
-    { title: "About", content: <div>About content here</div> },
-];
-
-const Tabs = () => {
-    const [activeTabIndex, setActiveTabIndex] = useState(0)
+const Tabs = ({ tabData }) => {
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
     return (
         <>
             <div className='flex justify-center border gap-10 p-2'>
                 {
+                    /* TAB GROUP (Wares, Guides, About) */
+
                     tabData.map((tab, index) => (
                         <div key={index}>
                             <button onClick={() => {
@@ -28,6 +24,8 @@ const Tabs = () => {
             </div>
             <div>
                 {
+                    /* TAB CONTENT */
+
                     tabData.map((tab, index) => (
                         activeTabIndex === index ? (
                             <TabPanel key={index}>
